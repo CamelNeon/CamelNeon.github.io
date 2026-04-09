2d552298f51f0a93d7353a6b953619aa1a4e2c01
dielsam <amaury.diels@arhs-spikeseed.com>
2026-04-09 12:58:57 +0200

    update vite config

--- noteblock-studio/vite.config.ts
+++ noteblock-studio/vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/steamerdev.fun/noteblock-studio/',
+    base: '/noteblock-studio/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
